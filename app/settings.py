b475908ecccf89e8e03a44a1ad62d3813954d9b3
xtina999 <avgustroder@gmail.com>
2024-05-10 16:52:41 +0300

    Added .env and .env.sample files to manage environment variables. The .env file contains sensitive information such as secret keys, which has been hidden.

--- app/settings.py
+++ app/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)9_i+n%(qsx@(gs2o3+8(r2p68+l@!*s2i$z8y&cf1ax5dy7^m"
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
